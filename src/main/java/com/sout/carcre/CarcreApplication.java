5a004c6e85318823054a93dad15b1550d2c6b9f0
ndd-maker <54309009+ndd-maker@users.noreply.github.com>
2020-02-08 11:48:46 +0800

    项目工程结构目录

---
+++ src/main/java/com/sout/carcre/CarcreApplication.java
@@ -0,0 +1,13 @@
+package com.sout.carcre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarcreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarcreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sout/carcre/CarcreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sout.carcre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarcreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
