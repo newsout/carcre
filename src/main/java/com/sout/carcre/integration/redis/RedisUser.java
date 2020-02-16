5137c8274c604902ff1ccce557275994f43f6a82
ndd <2287553830@qq.com>
2020-02-16 20:47:44 +0800

    增加redis切换数据库操作

--- src/main/java/com/sout/carcre/integration/redis/RedisUser.java
+++ src/main/java/com/sout/carcre/integration/redis/RedisUser.java
@@ -1,6 +1,7 @@
 package com.sout.carcre.integration.redis;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -18,10 +19,21 @@ public class RedisUser {
     @Autowired
     StringRedisTemplate stringRedisTemplate;
 
+    /*向redis不同库中存值*/
     public String cache(){
-        redisTemplate.opsForValue().set("manager","ndd",2, TimeUnit.MINUTES);
+        redisTemplate.opsForValue().set("manager","ndd");
         redisTemplate.opsForList().leftPush("k1",1);
         redisTemplate.opsForList().leftPush("k1",2);
+
+        LettuceConnectionFactory lettuceConnectionFactory = (LettuceConnectionFactory)redisTemplate.getConnectionFactory();
+        //切换数据库
+        lettuceConnectionFactory.setDatabase(2);
+        //如果不加以下一行代码，connectprovider中的connect配置一直都会是原来的database为0（默认数据库状态）
+        lettuceConnectionFactory.afterPropertiesSet();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        //如果不加以下一行代码,connect不会为null，就不会执行connectprovider中的配置代码
+        lettuceConnectionFactory.resetConnection();
+
         redisTemplate.opsForList().leftPush("k2",1);
         redisTemplate.opsForSet().add("set01",7,5,5,5);
         redisTemplate.opsForHash().put("hash01","子元素","value");
@@ -30,7 +42,6 @@ public class RedisUser {
         redisTemplate.opsForValue().set("2","ndd");
         redisTemplate.opsForValue().set("3","ndd");
         String data=stringRedisTemplate.opsForValue().get("1");
-        System.out.println(data);
         return data;
     }
 
